fe4927877353fa2b6244ee845037f8c8195c1e0f
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-03 19:03:50 +0000

    src

---
+++ src/main/kotlin/com/springboot/movieRatingKotlin/MovieRatingKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.springboot.movieRatingKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieRatingKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MovieRatingKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/springboot/movieRatingKotlin/MovieRatingKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springboot.movieRatingKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieRatingKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
